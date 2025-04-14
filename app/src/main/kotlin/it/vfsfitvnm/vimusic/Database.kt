664969f780a06f632231eccb41a0b6ff9d95c953
Daniel <daniel-sevillano@hotmail.com>
2025-04-14 10:59:27 +0200

    Fixing crash when opening media item menu in videos

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/Database.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/Database.kt
@@ -326,7 +326,7 @@ interface Database {
     fun search(query: String): Flow<List<Song>>
 
     @Query("SELECT albumId AS id, NULL AS name FROM SongAlbumMap WHERE songId = :songId")
-    fun songAlbumInfo(songId: String): Info
+    fun songAlbumInfo(songId: String): Info?
 
     @Query("SELECT id, name FROM Artist LEFT JOIN SongArtistMap ON id = artistId WHERE songId = :songId")
     fun songArtistInfo(songId: String): List<Info>
